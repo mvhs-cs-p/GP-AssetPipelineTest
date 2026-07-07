6153b9c5c33b871065c03716329ec70fc608d2dd
apetrek <apetrek@gmail.com>
2026-07-07 11:51:07 -0700

    Added boost to player movement.

--- DevTooling/Assets/Project/Scripts/PlayerInput.cs
+++ DevTooling/Assets/Project/Scripts/PlayerInput.cs
@@ -11,22 +11,28 @@ public class PlayerInput : MonoBehaviour
     {
         int xInput = 0;
         int yInput = 0;
+        int boostInput = 0;
+
+        if (Keyboard.current.leftShiftKey.isPressed)
+        {
+            boostInput = 1;
+        }
 
         if (Keyboard.current.wKey.isPressed)
         {
-            yInput += 1;
+            yInput += (1 + boostInput);
         }
         if (Keyboard.current.sKey.isPressed)
         {
-            yInput -= 1;
+            yInput -= (1 + boostInput);
         }
         if (Keyboard.current.aKey.isPressed)
         {
-            xInput -= 1;
+            xInput -= (1 + boostInput);
         }
         if (Keyboard.current.dKey.isPressed)
         {
-            xInput += 1;
+            xInput += (1 + boostInput);
         }
 
         MoveInput = new Vector2(xInput, yInput);
